3c679202bbaebdd2539a12bb93c6ce7875385904
xelfus23 <xelfus23@gmail.com>
2024-03-06 13:13:04 +0800

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function scrollLeft(){
+    var left = document.querySelector(".valo-videos");
+    left.scrollBy(350, 0)
+}
+
+function scrollRight(){
+    var right = document.querySelector(".valo-videos");
+    right.scrollBy(-350, 0)
+}
